2197ef4f4d6dd7cbb2c71a865022c908e503e4a7
Thomas Daede <daede003@umn.edu>
2019-08-19 21:52:17 -0700

    Fix up nmigen platform.

--- gateware/midiori_platform.py
+++ gateware/midiori_platform.py
@@ -10,28 +10,27 @@ class MidioriPlatform(LatticeICE40Platform):
         ("sync", 16e6),
     ]
     resources = [
-        Resource("sync", 0, Pins("63", dir="i"),
-                 extras={"GLOBAL": "1", "IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("rw", 0, Pins("100", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("exreset", 0, Pins("99", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("ioclk", 0, Pins("97", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("iddir", 0, Pins("96", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("iack4", 0,Pins("95", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("iack2", 0, Pins("94", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("dtack", 0, Pins("93", dir="o"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("irq4", 0, Pins("91", dir="o"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("irq2", 0, Pins("90", dir="o"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("lds", 0, Pins("1", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("as", 0, Pins("2", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("a", 0, Pins("3", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("b", 0, Pins("4", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("c", 0, Pins("7", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
+        Resource("sync", 0, Pins("63", dir="i")),
+        Resource("rw", 0, Pins("100", dir="i")),
+        Resource("exreset", 0, Pins("99", dir="i")),
+        Resource("ioclk", 0, Pins("97", dir="i")),
+        Resource("iddir", 0, Pins("96", dir="i")),
+        Resource("iack4", 0,Pins("95", dir="i")),
+        Resource("iack2", 0, Pins("94", dir="i")),
+        Resource("dtack", 0, Pins("93", dir="o")),
+        Resource("irq4", 0, Pins("91", dir="o")),
+        Resource("irq2", 0, Pins("90", dir="o")),
+        Resource("lds", 0, Pins("1", dir="i")),
+        Resource("as", 0, Pins("2", dir="i")),
+        Resource("a", 0, Pins("3", dir="i")),
+        Resource("b", 0, Pins("4", dir="i")),
+        Resource("c", 0, Pins("7", dir="i")),
 
-                 Resource("addr", 0, Pins("40 37 36 34 33 30 29 18 16 15 13 12 10 9 8 81 80 79 78 74 73 72 71", dir="i"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("data", 0, Pins("28 27 26 25 24 21 20 19", dir="io"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
+        Resource("addr", 0, Pins("40 37 36 34 33 30 29 18 16 15 13 12 10 9 8 81 80 79 78 74 73 72 71", dir="i")),
+        Resource("data", 0, Pins("28 27 26 25 24 21 20 19", dir="io")),
 
-                 Resource("xltr_oe", 0, Pins("51", dir="o"), extras={"IO_STANDARD": "SB_LVCMOS33"}),
-                 Resource("tx", 0, Pins("66", dir="o"), extras={"IO_STANDARD": "SB_LVCMOS33"})
+        Resource("xltr_oe", 0, Pins("51", dir="o")),
+        Resource("tx", 0, Pins("66", dir="o"))
 
     ]
     connectors = []
